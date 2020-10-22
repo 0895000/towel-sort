915d6b892b54f1fca7009e26803874046689d18b
Andrey Duka <70843050+0895000@users.noreply.github.com>
2020-10-22 16:52:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [7, 8, 9];
+const arr4 = arr1.concat(arr2).concat(arr3);
 
+
+arr4.sort(function(a, b){
+	return a - b;
+});
+
+console.log(arr4);
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
