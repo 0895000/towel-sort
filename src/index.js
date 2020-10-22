f4731076344ed7d6e1fef0eb20217e5a58e00a9d
Andrey Duka <70843050+0895000@users.noreply.github.com>
2020-10-22 19:02:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 module.exports = function towelSort (matrix) {
-  return [];
-}
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = [7, 8, 9];
-const arr4 = arr1.concat(arr2, arr3);
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+    const arr3 = [7, 8, 9];
+    const arr4 = arr1.concat(arr2, arr3);
 
 
 arr4.sort(function(a, b){
@@ -13,6 +10,5 @@ arr4.sort(function(a, b){
 });
 
 console.log(arr4);
-
-
-
+  return [];
+}
